c826582be5647afc49396fc5e3c18182ae641bdf
Kelvyn <carlos.kelvyn@hotmail.com>
2021-11-14 15:38:19 -0300

    Parte de manipulação web concluida

--- code/app.py
+++ code/app.py
@@ -1,11 +1,14 @@
+from typing import Text
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver: WebDriver
+
 
 #region Helpers
 def setup_driver():
@@ -16,13 +19,17 @@ def setup_driver():
     return driver  
     
 
-def change_tab():
+def change_tab(from_closed=False):
+    if(from_closed):
+        driver.switch_to.window(driver.window_handles[0])  
+        return    
+
     p = driver.current_window_handle
     chwd = driver.window_handles
     for w in chwd:
         if(w!=p):
             driver.switch_to.window(w)            
-        break
+            break
 
 
 def find_and_click_bytext(text):
@@ -38,7 +45,17 @@ def find_and_click_bylocator(by, locator):
 def find_and_write_input(by, locator, input_text):
     elem = driver.find_element(by, locator)
     elem.send_keys(input_text)
+
+
+def validate_closed_window(driver):
+    try:
+        driver.current_window_handle
+        return False
+    except:
+        return True
+
 #endregion
+
 
 #region Examples
 def commands_examples():
@@ -49,6 +66,7 @@ def commands_examples():
     elem.send_keys(Keys.RETURN)
     assert "No results found." not in driver.page_source
 #endregion
+
 
 def configure_metamask():
     change_tab()
@@ -82,13 +100,58 @@ def configure_wallet():
 
     find_and_click_bytext("Salvar")
 
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//span[@class='currency-display-component__suffix' and contains(text(),'BNB')]"))
+    )
+
+    assert "BNB" in element.text
+
+
+def connect_wallet_to_game():
+    driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe"))
+
+    find_and_click_bytext("MetaMask")
+
+    driver.switch_to.default_content()    
+
+    WebDriverWait(driver, 10).until(
+        EC.number_of_windows_to_be(2)
+    )
+
+    change_tab()
+
+    find_and_click_bytext("Próximo")
+
+    find_and_click_bylocator(By.XPATH, "//button[contains(text(),'Conectar')]")
+
+    WebDriverWait(driver, 10).until(
+        validate_closed_window
+    )      
+
+    WebDriverWait(driver, 10).until(
+        EC.number_of_windows_to_be(2)
+    )
+
+    change_tab(True)  
+
+    change_tab()
+    
+    find_and_click_bytext("Assinar")
+
+    change_tab(True)
+
 
 driver = setup_driver()
+
 configure_metamask()
 
-change_tab()
+driver.close()
+
+change_tab(True)
 
 driver.get("https://app.bombcrypto.io")
+
+connect_wallet_to_game()
 
 driver.close()
 

d81376540fbee043f50ae8d4c60744c298cb8cdb
Kelvyn <carlos.kelvyn@hotmail.com>
2021-11-16 12:14:32 -0300

    possivel_finalização

--- code/crypto_automation/image_processing/game_status_watcher.py
+++ code/crypto_automation/image_processing/game_status_watcher.py
@@ -30,8 +30,8 @@ class GameStatusWatcher:
 
         self.__connect_wallet_and_start()
 
-        #error_handling = Thread(self.__thread_safe, self.__verify_and_handle_game_error, self.__config['RETRY'].getint('verify_error'))
-        #newmap_handling = Thread(self.__thread_safe, self.__verify_and_handle_newmap, self.__config['RETRY'].getint('verify_newmap'))
+        error_handling = Thread(self.__thread_safe, self.__verify_and_handle_game_error, self.__config['RETRY'].getint('verify_error'))
+        newmap_handling = Thread(self.__thread_safe, self.__verify_and_handle_newmap, self.__config['RETRY'].getint('verify_newmap'))
         hero_handling = Thread(self.__thread_safe, self.__verify_and_handle_heroes_status, self.__config['RETRY'].getint('verify_heroes_status'))
 
 
@@ -50,7 +50,7 @@ class GameStatusWatcher:
 
 
     def __verify_and_handle_game_error(self):
-        error = self.__wait_until_match_is_found( self.__config['TEMPLATES']['error_message'], 2 )
+        error = self.__wait_until_match_is_found( self.__config['TEMPLATES']['error_message'], 2 , 0.05)
         if error:
             self.__find_and_click_by_template(self.__config['TEMPLATES']['ok_button'])
             self.__connect_wallet_and_start()
@@ -75,60 +75,28 @@ class GameStatusWatcher:
     def __click_all_work_buttons(self):
         result_match = self.__wait_all_until_match_is_found(self.__config['TEMPLATES']['work_button'], 10, 0.02)
 
-        if len(result_match) == 0:
+        count = 0
+        while len(result_match) == 0 and count <= 2:
             result_active_match = self.__wait_all_until_match_is_found(self.__config['TEMPLATES']['work_active_button'], 10, 0.02)
 
-            first_active_button_x, first_active_button_y = result_active_match[0]
             last_active_button_x, last_active_button_y  = result_active_match[len(result_active_match)-1]
-
-            zero_elem = self.webdriver.find_element_by_tag_name('body')
-            x_body_active_offset = -(zero_elem.size['width']/2)
-            y_body_active_offset = -(zero_elem.size['height']/2)+130
-
-            space_to_scroll = -(last_active_button_x - first_active_button_y)
-
-
-            iframe = self.webdriver.find_element_by_tag_name('iframe')
-
-            # switch to selected iframe
-            self.webdriver.switch_to.frame(iframe)
-
-            canvas_elem = self.webdriver.find_element_by_tag_name('canvas')
-
-
-            win32api.SetCursorPos((last_active_button_x, last_active_button_y))
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,last_active_button_x, last_active_button_y,0,0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,last_active_button_x, last_active_button_y,0,0)
-            #actions = ActionChains(self.webdriver)            
-            #actions.move_to_element_with_offset(canvas_elem, 300, 500).pause(1).click().pause(1).perform()
-            for x in range(100):  
-                win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, -1, 0)
-            #actions.move_to_element_with_offset(canvas_elem).pause(1).release().pause(1).perform()
-            #actions.move_to_element(zero_elem)
-            #actions.move_by_offset(x_body_active_offset, y_body_active_offset).perform()
-            #actions.move_by_offset(last_active_button_x, last_active_button_y).click_and_hold().pause(2).
-
-            self.webdriver.switch_to.default_content()
             
-        result_match = self.__wait_all_until_match_is_found(self.__config['TEMPLATES']['work_button'], 10, 0.02)
+            self.__click_and_scroll_down(last_active_button_x, last_active_button_y)            
+            
+            result_match = self.__wait_all_until_match_is_found(self.__config['TEMPLATES']['work_button'], 10, 0.02)
+
+            count +=1
+
 
         while len(result_match):
 
             for (x, y) in result_match:
                 self.__click_element_by_position(x, y)
-                time.sleep(0.3)
-
-            first_button_x, first_button_y = result_match[0]
+                time.sleep(0.5)
+            
             last_button_x, last_button_y = result_match[len(result_match)-1]
-
-            zero_elem = self.webdriver.find_element_by_tag_name('body')
-            x_body_offset = -(zero_elem.size['width']/2)
-            y_body_offset = -(zero_elem.size['height']/2)+130
-
-            actions = ActionChains(self.webdriver)
-            actions.move_to_element(zero_elem)
-            actions.move_by_offset(x_body_offset, y_body_offset).perform()
-            actions.move_by_offset(last_button_x, last_button_y).click_and_hold().move_by_offset(0, -(last_button_x - first_button_y)).release().perform()
+            
+            self.__click_and_scroll_down(last_button_x, last_button_y)
 
             result_match = self.__wait_all_until_match_is_found(self.__config['TEMPLATES']['work_button'], 10, 0.02)
 
@@ -150,7 +118,7 @@ class GameStatusWatcher:
                 time.sleep(0.3)
 
 
-    def __wait_until_match_is_found(self, template_path, timeout): 
+    def __wait_until_match_is_found(self, template_path, timeout, confidence_level = 0.1): 
         duration = 0
         result = None        
         template = cv2.imread(template_path)    
@@ -159,7 +127,7 @@ class GameStatusWatcher:
             time.sleep(1)
             duration += 1
             website_picture = self.__selenium_screenshot_to_opencv() 
-            result = self.image_helper.find_exact_match_position(website_picture, template)  
+            result = self.image_helper.find_exact_match_position(website_picture, template, confidence_level)  
 
         return result
 
@@ -200,7 +168,15 @@ class GameStatusWatcher:
         actions.move_to_element(zero_elem)
         actions.move_by_offset(x_body_offset, y_body_offset).perform()
         actions.move_by_offset(x_position, y_position).click().perform()
-        
+
+
+    def __click_and_scroll_down(self, x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x, y,0,0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x, y,0,0)   
+        for x in range(1200):  
+            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, -1, 0)
+
     #usage example: self.__thread_sensitive(method, 2, ['spam'], {'ham': 'ham'})
     def __thread_safe(self, method, retrytime, positional_arguments = None, keyword_arguments = None):
         while True:
@@ -214,3 +190,4 @@ class GameStatusWatcher:
                 else:
                     method()
             time.sleep(retrytime) 
+#endregion

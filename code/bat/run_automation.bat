da85e4f00fd82344e52bcca332833fb3e1376680
KelvynSouza <carlos.kelvyn@hotmail.com>
2021-11-17 18:48:47 -0300

    bat para rodar

---
+++ code/bat/run_automation.bat
@@ -0,0 +1,4 @@
+set venv_path=C:\Users\Kelvyn\Documents\dev\CryptoOcrAutomation\code\venv\Scripts
+set automation_path=C:\Users\Kelvyn\Documents\dev\CryptoOcrAutomation\code\crypto_automation
+
+cmd /k "cd %venv_path% & activate.bat & cd %automation_path% & python __main__.py"

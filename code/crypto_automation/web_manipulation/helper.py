1cee8216f2c3dd64ead069043294da6de82c403e
Kelvyn <carlos.kelvyn@hotmail.com>
2021-11-17 16:34:00 -0300

    Adição de protetor contra detecção de automação

--- code/crypto_automation/web_manipulation/helper.py
+++ code/crypto_automation/web_manipulation/helper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
+import undetected_chromedriver as uc
 
 class SeleniumHelper:  
     def __init__(self, config):
@@ -10,10 +10,18 @@ class SeleniumHelper:
 
     def setup_driver(self):
         chrome_options = Options()
+        chrome_options.add_argument('--profile-directory=Default')
+        chrome_options.add_argument("--incognito")
+        chrome_options.add_argument("--disable-plugins-discovery")
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_extension(self.__config['WEBDRIVER']['metamaskextension'])
         chrome_options.add_experimental_option("useAutomationExtension", False)
         chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+        driver = uc.Chrome(options=chrome_options)
+
         driver = webdriver.Chrome(self.__config['WEBDRIVER']['chromedriver'], options=chrome_options)
+        driver.delete_all_cookies()
         driver.implicitly_wait(self.__config['TIMEOUT'].getint('webscraping'))  
         driver.maximize_window()
         return driver  

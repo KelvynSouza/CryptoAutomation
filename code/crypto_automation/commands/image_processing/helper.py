3b42503951200cba1e8dacf6132d4dc60e67bebd
Kelvyn <carlos.kelvyn@hotmail.com>
2021-11-24 10:52:24 -0300

    Correção log imagem

--- code/crypto_automation/commands/image_processing/helper.py
+++ code/crypto_automation/commands/image_processing/helper.py
@@ -134,6 +134,4 @@ class ImageHelper:
         width = int(frame.shape[1] * percent/ 100)
         height = int(frame.shape[0] * percent/ 100)
         dim = (width, height)
-        if convert_grayscale:
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         return cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)

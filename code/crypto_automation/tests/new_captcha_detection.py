e9dda369c0a56e97414d61f9d2caca2f6f9f4fda
Kelvyn <carlos.kelvyn@hotmail.com>
2021-12-11 19:28:03 -0300

    Evolução

--- code/crypto_automation/tests/new_captcha_detection.py
+++ code/crypto_automation/tests/new_captcha_detection.py
@@ -106,154 +106,138 @@ class TestCaptchaSolver:
 
 
     def run(self):
-        loop = True
+        
 
         slide_button = self.__image_helper.wait_until_match_is_found(self.__windows_action_helper.take_screenshot, 
                                                                 [], self.__config['TEMPLATES']['captcha_slide'], self.__config['TIMEOUT'].getint('imagematching'), 
                                                                     0.05, False, True)
 
-        game_image = self.get_game_window_image()
-
-        x, y, w, h = self.get_captcha_window_contour(game_image)      
-
-        captcha_image = game_image[y:y+h,x:x+w]
+        captcha_x, captcha_y, captcha_w, captcha_h = self.get_captcha_window_contour(self.get_game_window_image())      
         
-        desktop_game_gray = cv2.cvtColor(captcha_image, cv2.COLOR_BGR2GRAY) 
-        _, thresh_image = cv2.threshold(desktop_game_gray, 91, 255, cv2.THRESH_BINARY_INV) 
+        for l in range(4): 
+            captcha_image = self.get_game_window_image()[captcha_y:captcha_y+captcha_h,captcha_x:captcha_x+captcha_w]
 
-        #Create a structure to aggregate incomplete elements
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
-        cv2.dilate(thresh_image,kernel,thresh_image, iterations = 1)
+            desktop_game_gray = cv2.cvtColor(captcha_image, cv2.COLOR_BGR2GRAY) 
+            _, thresh_image = cv2.threshold(desktop_game_gray, 91, 255, cv2.THRESH_BINARY_INV) 
 
-        # get the contours and hierarchy so that we can filter only the contours from the digits
-        contours, hierarchy  = cv2.findContours(thresh_image, cv2.RETR_CCOMP , cv2.CHAIN_APPROX_SIMPLE)
-        hierarchy = hierarchy[0] # get the actual inner list of hierarchy descriptions
-        
-        newContours = list()
-        for currentContour, currentHierarchy in zip(contours, hierarchy):  
-            # these are the innermost child components for the outermost,
-            # you should use currentHierarchy[3] < 0          
-            if currentHierarchy[2] < 0:                
-                newContours.append(currentContour)  
-        
-        #get numbers to validate
-        digits_to_validate = list()
-        for number in range(10):
-            template = cv2.imread(config['TEMPLATES'][f"simple_{number}"])
-            position = self.__image_helper.find_exact_match_position(template, captcha_image, True, 0.01)
-            if position:
-                digits_to_validate.append((number, position))
+            #Create a structure to aggregate incomplete elements
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+            cv2.dilate(thresh_image,kernel,thresh_image, iterations = 1)
 
-        digits_to_validate.sort(key=lambda tup: tup[1].x)
+            # get the contours and hierarchy so that we can filter only the contours from the digits
+            contours, hierarchy  = cv2.findContours(thresh_image, cv2.RETR_CCOMP , cv2.CHAIN_APPROX_SIMPLE)
+            hierarchy = hierarchy[0] # get the actual inner list of hierarchy descriptions
 
-        #fill numbers to extract for better extraction of the captcha 
-        cv2.drawContours(captcha_image, newContours, -1, (82,112,181), thickness=-1)
+            newContours = list()
+            for currentContour, currentHierarchy in zip(contours, hierarchy):  
+                # these are the innermost child components for the outermost,
+                # you should use currentHierarchy[3] < 0          
+                if currentHierarchy[2] < 0:                
+                    newContours.append(currentContour)  
 
-        '''
-        #timed loop
-        start_time = time.time()
-        seconds = 4
+            #get numbers to validate
+            digits_to_validate = list()
+            for number in range(10):
+                template = cv2.imread(config['TEMPLATES'][f"simple_{number}"])
+                position = self.__image_helper.find_exact_match_position(template, captcha_image, True, 0.01)
+                if position:
+                    digits_to_validate.append((number, position))
 
-        current_time = time.time()
-        elapsed_time = current_time - start_time 
+            digits_to_validate.sort(key=lambda tup: tup[1].x)
+                        
+            start_time = time.time()
+            seconds = 25 
+            #validate phases of captcha
+            while True:
+                #check timeout
+                current_time = time.time() 
+                elapsed_time = current_time - start_time
+                if elapsed_time > seconds:  
+                    print(f"Timeout, Captcha number not found")              
+                    break
+                
+                captcha_image = self.get_game_window_image()[captcha_y:captcha_y+captcha_h,captcha_x:captcha_x+captcha_w]
+                
+                #fill numbers to extract for better extraction of the captcha 
+                cv2.drawContours(captcha_image, newContours, -1, (82,112,181), thickness=-1) 
 
-        if elapsed_time > seconds:                
-                print("Finished iterating in: " + str(int(elapsed_time))  + " seconds")
-                break
-        '''
+                mask = cv2.inRange(captcha_image, (250,250,250), (255,255,255))
 
-        for x in range(10):                      
-            
-            #get white number on background
-            mask = cv2.inRange(captcha_image, (250,250,250), (255,255,255))
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))        
+                to_validate_number = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
 
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))        
-            to_validate_number = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
+                cleaning_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))    
+                to_locale_position = cv2.morphologyEx(mask, cv2.MORPH_OPEN, cleaning_kernel, iterations=3)
 
-            cleaning_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))    
-            to_locale_position = cv2.morphologyEx(mask, cv2.MORPH_OPEN, cleaning_kernel, iterations=3)
+                #remove noise from image
+                rect_contours = self.getting_rectangle_countours(to_locale_position, 0,20,0,20)
+                to_locale_position = self.draw_rectangles_in_image(to_locale_position, rect_contours)         
 
-            #remove noise from image
-            rect_contours = self.getting_rectangle_countours(to_locale_position, 0,20,0,20)
-            to_locale_position = self.draw_rectangles_in_image(to_locale_position, rect_contours)         
+                self.show_info(to_validate_number,  image_name="Image to validate number")
+                self.show_info(to_locale_position,  image_name="Image to get number position")
 
-            self.show_info(to_validate_number,  image_name="Image to validate number")
-            self.show_info(to_locale_position,  image_name="Image to get number position")
+                #Close gaps from corrupted numbers
+                #One idea is start with low x value for structure and increase until we have only 3 contours 
+                # if it turns into 2 or less, we give up the image and try the take another        
+                aux_y = 0
+                for y in range(55, 80, 2):
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1, y))
+                    to_locale_position_morph = cv2.morphologyEx(to_locale_position, cv2.MORPH_CLOSE, kernel)
+                    contours = self.getting_rectangle_countours(to_locale_position_morph, 90, 200, 5, 200)  
 
-            #Close gaps from corrupted numbers
-            #One idea is start with low x value for structure and increase until we have only 3 contours 
-            # if it turns into 2 or less, we give up the image and try the take another        
-            aux_y = 0
-            for y in range(55, 80, 2):
-                kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1, y))
-                to_locale_position_morph = cv2.morphologyEx(to_locale_position, cv2.MORPH_CLOSE, kernel)
-                contours = self.getting_rectangle_countours(to_locale_position_morph, 90, 200, 5, 200)  
+                    if len(contours) == 3:
+                        aux_y = y
+                        continue  
 
-                if len(contours) == 3:
-                    aux_y = y
-                    break  
+                temp_contours = list()
+                for x in range(1, 30, 2):
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(x, aux_y if aux_y > 0 else 60))
+                    to_locale_position_morph = cv2.morphologyEx(to_locale_position, cv2.MORPH_CLOSE, kernel)
+                    contours = self.getting_rectangle_countours(to_locale_position_morph, 90, 200, 5, 200)  
 
-            temp_contours = list()
-            for x in range(1, 30, 2):
-                kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(x, aux_y if aux_y > 0 else 60))
-                to_locale_position_morph = cv2.morphologyEx(to_locale_position, cv2.MORPH_CLOSE, kernel)
-                contours = self.getting_rectangle_countours(to_locale_position_morph, 90, 200, 5, 200)  
+                    if len(contours) == 3:
+                        temp_contours = contours                
+                    elif len(contours) < 3:
+                        if len(temp_contours) > 0:
+                            contours = temp_contours
+                            continue
+                        
+                if len(contours) < 3:
+                    print("Error getting numbers position")
+                    continue
 
-                if len(contours) == 3:
-                    temp_contours = contours                
-                elif len(contours) < 3:
-                    if len(temp_contours) > 0:
-                        contours = temp_contours
-                        break
-                    
-            if len(contours) < 3:
-                raise Exception("Error getting numbers position")
+                #fix border if its near the image's border
+                contours = [(contour[0], 20 if contour[1] == 0 else contour[1], contour[2], contour[3]) for contour in contours]
 
-            #fix border if its near the image's border
-            contours = [(contour[0], 20 if contour[1] == 0 else contour[1], contour[2], contour[3]) for contour in contours]
+                #Sort contours position to be equals to text position
+                contours.sort(key=lambda tup: tup[0])       
 
-            #Sort contours position to be equals to text position
-            contours.sort(key=lambda tup: tup[0])       
+                self.show_info(to_locale_position_morph)
+                drawn_image = np.copy(captcha_image)
+                self.draw_rectangles_in_image(drawn_image, contours,  (255,0,0), 2)
+                self.show_info(drawn_image)
 
-            self.show_info(to_locale_position_morph)
-            drawn_image = np.copy(captcha_image)
-            self.draw_rectangles_in_image(drawn_image, contours,  (255,0,0), 2)
-            self.show_info(drawn_image)
-
-            for i in range(0, len(digits_to_validate)):
-                retry = True
-                start_time = time.time()
-                seconds = 10
-                while retry:                    
+                for i in range(0, len(digits_to_validate)):
                     #prepare template to compare
-                    template = cv2.imread(config['TEMPLATES'][digits_to_validate[i][0]])
-                    grey_digit_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY) 
-                    _, thresh_digit_template = cv2.threshold(grey_digit_template, 200, 255, cv2.THRESH_BINARY)            
+                    template = cv2.imread(config['TEMPLATES'][f"complex_{digits_to_validate[i][0]}"])
+
+                    grey_digit_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)                     
+                    _, thresh_digit_template = cv2.threshold(grey_digit_template, 200, 255, cv2.THRESH_BINARY) 
 
                     x,y,w,h = contours[i]
-                    resized_template = cv2.resize(thresh_digit_template, (w,h) , interpolation = cv2.INTER_LANCZOS4)
 
+                    resized_template = cv2.resize(thresh_digit_template, (w,h) , interpolation = cv2.INTER_LANCZOS4)
                     image_to_validate = to_validate_number[y:y+h, x:x+w] + resized_template
                     self.show_info(image_to_validate)
 
                     result = self.__image_helper.find_exact_match_position(resized_template, image_to_validate, False, 0.2)
 
-                    current_time = time.time()
-                    elapsed_time = current_time - start_time 
-
                     if result:
                         print(f"Success letter: {digits_to_validate[i]}")
-                    elif elapsed_time > seconds:  
-                        print(f"Letter '{digits_to_validate[i]}' not found")              
+                    else:
                         break
-                        
 
-
-
-
-
-
-
+            
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'

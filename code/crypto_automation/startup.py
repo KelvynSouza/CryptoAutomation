8851b3cfa68ca43606e3b20a7fac5e5d467004f5
Kelvyn <carlos.kelvyn@hotmail.com>
2021-12-20 16:58:16 -0300

    .

--- code/crypto_automation/startup.py
+++ code/crypto_automation/startup.py
@@ -1,5 +1,6 @@
 import configparser
 import logging
+import threading
 import traceback
 import keyring
 from app.commands.game_status_manager import GameStatusManager
@@ -10,13 +11,31 @@ import crypto_automation.app.shared.log_helper as log
 
 def run():   
     log.warning('Starting automation.', chat)
+    
+    secure_passwords()
+    lock = threading.Lock()
+    start_browser('chrome', lock)    
+    
 
+def secure_passwords():
+    if config['SECURITY'].getboolean('ispasswordsecured') == False:
+        keyring.set_password(config['SECURITY']['serviceid'], "secret_password_chrome", config['LOGIN']['password_chrome'])
+        keyring.set_password(config['SECURITY']['serviceid'], "secret_password_edge", config['LOGIN']['password_edge'])
+        config['LOGIN']['password_chrome'] = "Secured"
+        config['LOGIN']['password_edge'] = "Secured"
+        config['SECURITY']['ispasswordsecured'] = "True"
+        with open(config['COMMON']['settings_name'], 'w') as configfile:
+            config.write(configfile)
+    
+
+def start_browser(browser: str, lock: threading.Lock):
     try:
-        secure_passwords()
-
-        game_watcher = GameStatusManager(config, chat)
+        config['TEMPLATES']['browser_images_path'] = config['TEMPLATES'][f'{browser}_images_path'] 
+        config['WEBDRIVER'] = config[f'{browser.upper()}_DRIVER']
+        game_watcher = GameStatusManager(config, f"secret_password_{browser}", chat, lock)
         game_watcher.start_game()
-    except BaseException as ex:
+
+    except BaseException:
         global error
         error += 1
         log.error('Error:' + traceback.format_exc())
@@ -24,25 +43,14 @@ def run():
 
         windows_helper = WindowsActionsHelper(config)
 
-        is_browser_open = windows_helper.process_exists(config['WEBDRIVER']['chrome_exe_name'])
+        is_browser_open = windows_helper.process_exists(config['WEBDRIVER']['exe_name'])
 
         if is_browser_open:
-            windows_helper.kill_process(config['WEBDRIVER']['chrome_exe_name'])
+            windows_helper.kill_process(config['WEBDRIVER']['exe_name'])
 
         if error < 10:
-            run()    
-    
+            start_browser() 
 
-def secure_passwords():
-        if config['SECURITY'].getboolean('ispasswordsecured') == False:
-            keyring.set_password(config['SECURITY']['serviceid'], "secret_password", config['LOGIN']['newpassword'])
-
-            config['LOGIN']['newpassword'] = "Secured"
-            config['SECURITY']['ispasswordsecured'] = "True"
-
-            with open(config['COMMON']['settings_name'], 'w') as configfile:
-                config.write(configfile)
-    
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 config.read( "settings.ini")

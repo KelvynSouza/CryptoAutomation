22e6aaf7e499e68526521ca1a49c7416c6b7cc72
hideyuky <dho.diogo@gmail.com>
2022-01-05 23:13:00 -0300

    timeout

--- code/crypto_automation/app/commands/game_status_manager.py
+++ code/crypto_automation/app/commands/game_status_manager.py
@@ -83,7 +83,7 @@ class GameStatusManager:
 
         self.__find_and_click_by_template(self.__config['TEMPLATES']['metamask_unlock_button'], 0.02)
 
-        if self.__image_helper.wait_until_match_is_found(self.__windows_action_helper.take_screenshot, [], self.__config['TEMPLATES']['metamask_sign_button'], 5, 0.05):
+        if self.__image_helper.wait_until_match_is_found(self.__windows_action_helper.take_screenshot, [], self.__config['TEMPLATES']['metamask_sign_button'], 10, 0.05):
             self.__find_and_click_by_template(self.__config['TEMPLATES']['metamask_sign_button']) 
         else:  
             self.__find_and_click_by_template(self.__config['TEMPLATES']['metamask_pending'])
